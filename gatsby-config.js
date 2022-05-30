766901c0ccf39583b46444cbd475d58085b3c930
TheShy <theshy.code@gmail.com>
2022-05-30 17:07:20 +0400

    Branding 😎

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,14 +1,14 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Starter Blog`,
+    title: `Personal Blog`,
     author: {
-      name: `Kyle Mathews`,
-      summary: `who lives and works in San Francisco building useful things.`,
+      name: `0xShy101`,
+      summary: `Personal Blog for my personal thoughts`,
     },
-    description: `A starter blog demonstrating what Gatsby can do.`,
+    description: `Personal Blog for my personal thoughts`,
     siteUrl: `https://gatsbystarterblogsource.gatsbyjs.io/`,
     social: {
-      twitter: `kylemathews`,
+      twitter: `@0xShy101`,
     },
   },
   plugins: [
